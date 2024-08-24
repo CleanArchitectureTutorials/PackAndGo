9a2eb2e19b2553435575636e2b06e3c32dcf934e
Lars Appel <lasse.appel@gmail.com>
2024-08-24 17:15:45 +0200

    Add project solution according to Project Setup

---
+++ src/PackAndGo.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PackAndGo.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
