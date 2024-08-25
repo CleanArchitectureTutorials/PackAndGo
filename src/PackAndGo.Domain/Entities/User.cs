b1fb3db597c5885e479cd118734ceafb8421c703
Lars Appel <lasse.appel@gmail.com>
2024-08-25 14:03:37 +0200

    Refactor the User Entity

--- src/PackAndGo.Domain/Entities/User.cs
+++ src/PackAndGo.Domain/Entities/User.cs
@@ -1,13 +1,30 @@
+using PackAndGo.Domain.Common;
+using PackAndGo.Domain.ValueObjects;
+
 namespace PackAndGo.Domain.Entities;
 
-public class User
+public class User : Entity
 {
-   public Guid Id { get; private set; }
-   public string Email { get; private set; }
+    public Email Email { get; private set; }
 
-   public User(Guid id, string email)
-   {
-       Id = id;
-       Email = email;
-   }
+    private User(Guid id, Email email)
+    {
+        Id = id;
+        Email = email;
+    }
+
+    public static User Create(string email)
+    {
+        return new User(Guid.NewGuid(), new Email(email));
+    }
+
+    public static User Load(Guid id, string email)
+    {
+        return new User(id, new Email(email));
+    }
+
+    public void ChangeEmail(string email)
+    {
+        Email = new Email(email);
+    }
 }
