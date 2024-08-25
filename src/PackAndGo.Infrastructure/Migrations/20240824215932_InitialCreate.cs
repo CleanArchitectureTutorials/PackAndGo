619af103c1345bef481197c61db6cd7bbc5ecaac
Lars Appel <lasse.appel@gmail.com>
2024-08-25 09:45:07 +0200

    Create the Database

---
+++ src/PackAndGo.Infrastructure/Migrations/20240824215932_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace PackAndGo.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email" },
+                values: new object[,]
+                {
+                    { new Guid("2fc56300-17dd-4a48-9793-363f14f29a30"), "jane.doe@test.com" },
+                    { new Guid("51181cb1-4e76-4402-aee6-aac3405ca654"), "john.doe@test.com" },
+                    { new Guid("8a468c35-db94-4e17-ae84-a7b25f581e8f"), "jacob.doe@test.com" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
