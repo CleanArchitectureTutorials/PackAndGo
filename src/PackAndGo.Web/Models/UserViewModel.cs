edcf50e5bd70986e0bcf5294f87ab683529d7a07
Lars Appel <lasse.appel@gmail.com>
2024-08-27 17:54:02 +0200

    Adding Client-Side Validation for Email

--- src/PackAndGo.Web/Models/UserViewModel.cs
+++ src/PackAndGo.Web/Models/UserViewModel.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PackAndGo.Web.Models;
 
 public class UserViewModel
 {
    public Guid Id { get; set; }
+   
+   [Required(ErrorMessage = "Email is required.")]
+   [EmailAddress(ErrorMessage = "Invalid email address.")]
    public string Email { get; set; } = string.Empty;
 }
